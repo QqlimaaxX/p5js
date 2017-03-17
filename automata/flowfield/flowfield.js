5af5b047b23be768d9e579bde07a74a33caa04f9
QqlimaaxX <mohitkarangiya@gmail.com>
2017-03-17 23:55:14 +0530

    adding three dimensional perlin noise
    here time is the third dimension

--- automata/flowfield/flowfield.js
+++ automata/flowfield/flowfield.js
@@ -3,7 +3,7 @@ function Flowfield(){
 	//in a grid, have them point in some directions
 	//and have some resolution set for division
 	//of the display into the grid
-	this.resolution = 50;
+	this.resolution = 100;
 	this.cols=width/this.resolution;
 	this.rows=height/this.resolution;
 	this.grid=[];
@@ -14,6 +14,7 @@ function Flowfield(){
 		for (var j=0;j<this.rows;j++){
 			var vec = p5.Vector.fromAngle(map(noise(i*0.2,j*0.2),0,1,0,2*PI));
 			// var vec = createVector(width/2-(i+0.5)*this.resolution,height/2-(j+0.5)*this.resolution)
+			// var vec = p5.Vector.fromAngle(random(-PI/4,PI/4));
 			this.grid[i].push(vec);
 		}
 	}
@@ -33,6 +34,18 @@ function Flowfield(){
 			}	
 		}
 	}
+
+	this.update=function(){
+		for(var i=0; i<this.cols;i++){
+			for (var j=0;j<this.rows;j++){
+				var vec = p5.Vector.fromAngle(map(noise(i*0.2,j*0.2,frameCount*0.02),0,1,0,2*PI));
+				// var vec = createVector(width/2-(i+0.5)*this.resolution,height/2-(j+0.5)*this.resolution)
+				// var vec = p5.Vector.fromAngle(random(-PI/4,PI/4));
+				this.grid[i][j]=vec;
+			}
+		}
+	}
+
 	this.lookup=function(loc){
 			// console.log(loc.x,loc.y);
 			var ref_vect = this.grid[Math.floor(loc.x/this.resolution)][Math.floor(loc.y/this.resolution)]
