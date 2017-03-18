c192f7522fbc4e375b74fe8d1aeed3ccd4d340e7
QqlimaaxX <mohitkarangiya@gmail.com>
2017-03-18 12:25:35 +0530

    adding varying strokewight

--- particles/sketch.js
+++ particles/sketch.js
@@ -1,7 +1,7 @@
 var particles = [];
 var count = 100;
-var min_len = 125;
-var alp,gap;
+var min_len = 125*125;
+var alp,gap,stkw;
 var fro,to,mid;
 
 function setup(){
@@ -20,18 +20,25 @@ function draw(){
 		var pos1=particles[i].pos;
 		for(var j=i;j<particles.length;j++){
 			var pos2=particles[j].pos;
-			var gap = dist(pos1.x,pos1.y,pos2.x,pos2.y);
+			// gap = dist(pos1.x,pos1.y,pos2.x,pos2.y);
+			gap = Math.pow((pos1.x-pos2.x),2) + Math.pow((pos2.y-pos1.y),2);
 			if(gap<min_len){
 				alp = map(gap,0,min_len,1,0);
+				stkw = map(gap,1,min_len,2,0);
 				stroke('rgba(0,0,0,'+alp+')');
+				strokeWeight(stkw);
 				line(pos1.x,pos1.y,pos2.x,pos2.y);
 			}
 		}
 		//also checking the distance of particle[i] with mouse's pos
-		gap=dist(pos1.x,pos1.y,mouseX,mouseY);
+		// gap=dist(pos1.x,pos1.y,mouseX,mouseY);
+		gap = Math.pow((pos1.x-mouseX),2) + Math.pow((mouseY-pos1.y),2);
+
 		if(gap<min_len){
 				alp = map(gap,0,min_len,1,0);
+				stkw = map(gap,1,min_len,1,5);
 				stroke('rgba(0,0,0,'+alp+')');
+				strokeWeight(stkw);
 				line(pos1.x,pos1.y,mouseX,mouseY);
 		}
 	}
